d9220cfec0563c75b3640f1c3778fbc3456b0318
Nicolas Raoul <nicolas.raoul@gmail.com>
2022-01-02 18:16:34 +0900

    Changed namespace

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -cp build/libs/coordinates2country.jar org.github.coordinates2country.Coordinates2Country "$@"
+java -cp build/libs/coordinates2country.jar io.github.coordinates2country.Coordinates2Country "$@"
